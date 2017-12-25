85fbac36b2e5fffc62b03f8023c075e9fa0a6397
Nic Newdigate <nicnewdigate@gmail.com>
2017-12-25 21:20:49 +0000

    minor correction

--- ObservableMessaging.IbmMq.Console/Program.cs
+++ ObservableMessaging.IbmMq.Console/Program.cs
@@ -8,7 +8,7 @@ namespace ObservableMessaging.IbmMq.ConsoleApp
     {
         static void Main(string[] args)
         {
-            IObservable<MQMessage> inbound = new InboundMessageQueue("QM1", "DEV.QUEUE.1", host: "johns-mac-pro.local", port: 1414, channel: "DEV.APP.SVRCONN");
+            IObservable<MQMessage> inbound = new InboundMessageQueue("QM1", "DEV.QUEUE.1", host: "hostname.local", port: 1414, channel: "DEV.APP.SVRCONN");
             IObservable<string> stringObservable = new MQStringAdapterSubject(inbound);
             stringObservable.Subscribe(mqmessage => {
                 Console.WriteLine($"Received message: {mqmessage}");
